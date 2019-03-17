df393f3a61782ef881f79577c33d59bf3bcb521e
Luca Giacometti <luca.giacometti@barsanti.edu.it>
2019-03-18 00:49:12 +0100

    Blockworld alfa #2
    Code optimization.

---
+++ Assets/Script/TreeSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    class TreeSpawn : MonoBehaviour
+    {
+        /// <summary>
+        /// Place the second layer of leaves (starting from the top) to a tree.
+        /// </summary>
+        /// <param name="pos">position of the second layer wood log of the tree</param>
+        /// <returns>array of objects not placed in the enviroment yet</returns>
+        public static GameObject[] CreateLeavesLayer2(GameObject[] blocks, Vector3 pos)
+        {
+            GameObject leavesBlock1 = Instantiate(blocks[4], new Vector3(pos.x + 1, pos.y, pos.z), Quaternion.identity);
+            GameObject leavesBlock2 = Instantiate(blocks[4], new Vector3(pos.x - 1, pos.y, pos.z), Quaternion.identity);
+            GameObject leavesBlock3 = Instantiate(blocks[4], new Vector3(pos.x, pos.y, pos.z + 1), Quaternion.identity);
+            GameObject leavesBlock4 = Instantiate(blocks[4], new Vector3(pos.x, pos.y, pos.z - 1), Quaternion.identity);
+            return new GameObject[] { leavesBlock1, leavesBlock2, leavesBlock3, leavesBlock4 };
+        }
+    }
+}
--- Assets/Script/WorldGeneration.cs
+++ Assets/Script/WorldGeneration.cs
@@ -13,14 +13,14 @@ public class WorldGeneration : MonoBehaviour
     public GameObject[] blocks;
     //private fields
     private new Transform transform;        //finds the parent by tag
-    private Assets.Script.Tree trees;        //tree class
+    //private Assets.Script.TreeSpawn trees;        //tree class
 
     // Start is called before the first frame update
     void Start()
     {
         //seed = Random.Range(100000, 999999);
         transform = GameObject.FindGameObjectWithTag("Enviroment").transform;
-        trees = new Assets.Script.Tree(blocks);
+        //trees = new Assets.Script.TreeSpawn();
         GenerateWorld();
     }
 
@@ -36,7 +36,7 @@ public class WorldGeneration : MonoBehaviour
         {
             for (int x = 0; x < xLenght; x++)
             {
-                int dirtLayers = Random.Range(3, 5);
+                int dirtLayers = Random.Range(3, 6);
                 int yLimit = (int)(Mathf.PerlinNoise(((x / 2) + seed) / terDetail, (z / 2 + seed) / terDetail) * terHeight);
                 for (int y = 0; y < yLimit; y++)
                 {
@@ -99,21 +99,21 @@ public class WorldGeneration : MonoBehaviour
     /// Place leaves to the tree depending on its height
     /// </summary>
     /// <param name="height">height of the tree in blocks</param>
-    /// <param name="pos">position of the top log of wood of the tree</param>
+    /// <param name="pos">position ON TOP of the top log of wood of the tree</param>
     private void PlaceTreeLeaves(int height, Vector3 pos)
     {
         if (height == 6)
         {
-            for (int y = (int)(pos.y + 1); y > (int)(pos.y - 1); y--)
+            for (int y = (int)(pos.y); y > (int)(pos.y - 2); y--)
             {
-                if (y == pos.y + 1)
+                if (y == pos.y)
                 {
                     GameObject leavesBlock = Instantiate(blocks[4], new Vector3(pos.x, y, pos.z), Quaternion.identity);
                     leavesBlock.transform.SetParent(transform);
                 }
-                else if (y == pos.y)
+                else if (y == pos.y - 1)
                 {
-                    trees.CreateLeavesLayer2()
+                    Assets.Script.TreeSpawn.CreateLeavesLayer2(blocks, new Vector3(pos.x, y, pos.z));
                     /*GameObject leavesBlock1 = Instantiate(blocks[4], new Vector3(pos.x + 1, y, pos.z), Quaternion.identity);
                     leavesBlock1.transform.SetParent(transform);
                     GameObject leavesBlock2 = Instantiate(blocks[4], new Vector3(pos.x - 1, y, pos.z), Quaternion.identity);
